f9ec9746737941f00e7ba46eccce7750b7f94938
Abhishek Kumar Yadav <45727674+coderaky@users.noreply.github.com>
2020-05-12 15:05:58 +0530

    Add files via upload

---
+++ src/firebase/config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBbnJXzGRQhTLhF9LWzxP2NClLImnJLgAU",
+  authDomain: "newsyreact-716c6.firebaseapp.com",
+  databaseURL: "https://newsyreact-716c6.firebaseio.com",
+  projectId: "newsyreact-716c6",
+  storageBucket: "newsyreact-716c6.appspot.com",
+  messagingSenderId: "87774534787",
+  appId: "1:87774534787:web:22dfc0eddc5f774d9acfb6",
+  measurementId: "G-W7XNNYBL5K"
+};
+export default firebaseConfig;
